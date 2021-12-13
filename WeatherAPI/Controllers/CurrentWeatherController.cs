8bd4efc28e4eadb8fbc60c8ce826231b4020105c
longnh-uit <longnh.0601@gmail.com>
2021-12-13 22:05:16 +0700

    add current weather api

---
+++ WeatherAPI/Controllers/CurrentWeatherController.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WeatherAPI.Models;
+using MongoDB.Driver;
+using WeatherAPI.App_Start;
+using MongoDB.Bson;
+
+namespace WeatherAPI.Controllers
+{
+    public class CurrentWeatherController : ApiController
+    {
+        private MongoDBContext dBContext;
+        private IMongoCollection<CurrentWeatherModel> currentWeatherCollection;
+
+        public CurrentWeatherController()
+        {
+            dBContext = new MongoDBContext();
+            currentWeatherCollection = dBContext.database.GetCollection<CurrentWeatherModel>("currentWeather");
+        }
+
+        // GET: api/updateweather
+        [Route("api/updateweather")]
+        [HttpGet]
+        async public Task<IHttpActionResult> UpdateWeather()
+        {
+            var client = new HttpClient();
+            string[] locations = new string[] { "Moscow", "London", "Ha Noi", "Thanh pho Ho Chi Minh", "Dubai" };
+            foreach (string location in locations)
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://community-open-weather-map.p.rapidapi.com/find?q={location}&cnt=1&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric"),
+                    Headers =
+                {
+                    { "x-rapidapi-host", "community-open-weather-map.p.rapidapi.com" },
+                    { "x-rapidapi-key", "b99908da4bmsh2b013b728fd8ad3p112d00jsn94d5bec39aa1" },
+                },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    String rawJSON = await response.Content.ReadAsStringAsync();
+                    rawJSON = rawJSON.Substring(rawJSON.IndexOf("list") + 7);
+                    rawJSON = rawJSON.Remove(rawJSON.Length - 2);
+                    CurrentWeatherModel result = JsonConvert.DeserializeObject<CurrentWeatherModel>(rawJSON);
+                    var replaceResult = currentWeatherCollection.ReplaceOne(x => x.name == location, result, new ReplaceOptions() { IsUpsert = true });
+                }
+
+            }
+
+            return Ok();
+        }
+
+        // GET: api/currentweather?name=ejafiesf
+        [Route("api/currentweather")]
+        [HttpGet]
+        public IHttpActionResult Get(string name)
+        {
+            try
+            {
+                var result = currentWeatherCollection.Find(x => x.name == name).FirstOrDefault();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+    }
+}
---
+++ WeatherAPI/Models/CurrentWeatherModel.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WeatherAPI.Models
+{
+    public class CurrentWeatherModel
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
+        public string name { get; set; }
+        public class CoordClass
+        {
+            public double lat { get; set; }
+            public double lon { get; set; }
+        }
+        public CoordClass coord { get; set; }
+        public class MainClass
+        {
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+            public double pressure { get; set; }
+            public double humidity { get; set; }
+        }
+        public MainClass main { get; set; }
+        public Int64 dt { get; set; }
+        public class WindClass
+        {
+            public double speed { get; set; }
+            public double deg { get; set; }
+        }
+        public WindClass wind { get; set; }
+        public class SysClass
+        {
+            public string country { get; set; }
+        }
+        public SysClass sys { get; set; }
+        public class RainClass
+        {
+            [JsonProperty("1h")]
+            public double _1h { get; set; }
+        }
+        public RainClass rain { get; set; }
+        public class SnowClass
+        {
+            [JsonProperty("1h")]
+            public double _1h { get; set; }
+        }
+        public SnowClass snow { get; set; }
+        public class CloudsClass
+        {
+            public double all { get; set; }
+        }
+        public CloudsClass clouds { get; set; }
+        public class WeatherClass
+        {
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+        public WeatherClass[] weather { get; set; }
+
+    }
+}
